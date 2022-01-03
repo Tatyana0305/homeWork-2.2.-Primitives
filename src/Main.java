7601157bfca7103a95426705ab10d303f66bfe19
Tatyana Bondarenko <tanya_kursavka@mail.ru>
2022-01-03 23:46:25 +0300

    Formula change

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int startingAmount = 100;
-        int paymentAmount = 1100;
+        int paymentAmount = 1200;
         int bonus = paymentAmount / 100;
-        int totalAmount = startingAmount + paymentAmount;
-        System.out.println("Сумма счета после пополнения составила " + totalAmount + " рублей");
+        int totalAmount1 = startingAmount + paymentAmount + bonus;//итоговая сумма при пополнении свыше 1000 руб
+        int totalAmount2 = startingAmount + paymentAmount;//итоговая сумма при пополнении менее 1000 руб
         if (paymentAmount > 1000) {
+            System.out.println("Сумма счета после пополнения составила " + totalAmount1 + " рублей, включая бонус");
             System.out.println("Ваш бонус " + bonus + " рублей");
         } else {
+            System.out.println("Сумма счета после пополнения составила " + totalAmount2 + " рублей");
             System.out.println("Ваш бонус 0 рублей");
 
         }
